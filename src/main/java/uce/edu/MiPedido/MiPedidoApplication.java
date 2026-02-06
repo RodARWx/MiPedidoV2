2b0ecc879d8cb1a225f8b6f8cfd652ea0c32468a
Arrow <Arrow@DESKTOP-J3IMT0M>
2026-02-06 17:25:35 -0500

    MiPedido inicio v2

---
+++ src/main/java/uce/edu/MiPedido/MiPedidoApplication.java
@@ -0,0 +1,13 @@
+package uce.edu.MiPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uce/edu/MiPedido/MiPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package uce.edu.MiPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
