b1b2b833258d00444fb4f6ae4e03d74b627ee466
Arrow <Arrow@DESKTOP-J3IMT0M>
2026-02-06 20:11:24 -0500

    se agregó el index y HomeController

---
+++ src/main/java/uce/edu/MiPedido/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package uce.edu.MiPedido.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
+
